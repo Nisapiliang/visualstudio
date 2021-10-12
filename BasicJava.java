b2b23878d6bd4334e3f731543f2245ad3d0e9393
Nisapiliang <khairunnisa.kn369@gmail.com>
2021-10-12 12:51:37 +0700

    comit pertama

---
+++ BasicJava.java
@@ -0,0 +1,26 @@
+public class BasicJava {
+    public static void main(String[] args){
+        String firstName, lastName, alamat, MakananKesukaan, Hobby, nomerhp;
+        int age;
+        double tinggi;
+
+        firstName = "Khoirunnisyah ";
+        lastName = "Piliang ";
+        alamat = "Medan ";
+        nomerhp = "082268551722 ";
+        age = 21;
+        tinggi = 150.5;
+        MakananKesukaan ="Sate ";
+        Hobby = "Renang ";
+
+        System.out.println("firstName= "+  "Khoirunnisyah");
+        System.out.println("lastName= " + "Piliang");
+        System.out.println("age= " + "21");
+        System.out.println("tinggi= " + "150.5");
+        System.out.println("alamat= " + "Medan");
+        System.out.println("nomerhp= " + "082268551722");
+        System.out.println("MakananKesukaan= " + "Sate");
+        System.out.println("Hobby= " + "Renang");
+
+    }
+}
